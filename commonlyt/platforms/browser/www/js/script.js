01dcf72cf8bf8329acee8cd7f35e35a1ab5fed56
surajsinghnegi009 <surajsinghnegi009@gmail.com>
2018-08-17 17:21:51 +0530

    1

--- commonlyt/platforms/browser/www/js/script.js
+++ commonlyt/platforms/browser/www/js/script.js
@@ -21,7 +21,7 @@ if( /(android)/i.test(navigator.userAgent) ) {
  
 if(AdMob) AdMob.createBanner( {
     // isTesting:true, //Remove this Before publishing your app
-    adId:admobid.banner, 
+    adId:admobid.banner,
     position:AdMob.AD_POSITION.BOTTOM_CENTER, 
     autoShow:true} );
   if(AdMob) AdMob.prepareInterstitial( {adId:admobid.interstitial, autoShow:false} );
